1c804b4bb25fb2905238a13390c3b1c8bf8b3164
mbusokotobewrk@gmail.com <mbusokotobewrk@gmail.com>
2021-09-17 19:34:06 +0200

    Project Success Created

---
+++ src/main/java/za/ac/cput/group23/inventory_system_group23_jversion/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.group23.inventory_system_group23_jversion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
